f4acece3bd3929839f6cbed9199dcde2e3fce10b
suraj shahdeo <105949766+surajshahdeo@users.noreply.github.com>
2025-08-29 17:04:12 +0530

    3rd commit

--- demoForGithub/src/sample/demo3.java
+++ demoForGithub/src/sample/demo3.java
@@ -2,7 +2,7 @@ package sample;
 
 public class demo3 {
 	public static void main(String[] args) {
-		System.out.println("hello qspider i m here");
+		System.out.println("hello qspider guys ");
 	}
 
 }
